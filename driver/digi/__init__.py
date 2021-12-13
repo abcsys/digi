9f859d414121b1624824d816eca174927cc13fbf
silveryfu <silveryfu@gmail.com>
2021-12-12 20:21:42 -0800

    allow default gvr in __init__
    Signed-off-by: silveryfu <silveryfu@gmail.com>

--- driver/digi/__init__.py
+++ driver/digi/__init__.py
@@ -8,19 +8,13 @@ logger = logging.getLogger(__name__)
 log_level = int(os.environ.get("LOGLEVEL", logging.INFO))
 logger.setLevel(log_level)
 
-if os.environ.get("TEST", False):
-    os.environ["GROUP"] = "digi.dev"
-    os.environ["VERSION"] = "v1"
-    os.environ["PLURAL"] = "tests"
-    os.environ["NAME"] = "t"
-
 # digi metadata and identifiers
-g = group = os.environ["GROUP"]
-v = version = os.environ["VERSION"]
-r = resource = os.environ["PLURAL"]
-n = name = os.environ["NAME"]
+g = group = os.environ.get("GROUP", "digi.dev")
+v = version = os.environ.get("VERSION", "v1")
+r = resource = os.environ.get("PLURAL", "tests")
+n = name = os.environ.get("NAME", "test")
 ns = namespace = os.environ.get("NAMESPACE", "default")
-auri = (g, v, r, n, ns)
+duri = auri = (g, v, r, n, ns)
 pool_provider = os.environ.get("POOL_PROVIDER", "zed")
 
 # digi modules; force init
