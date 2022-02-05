9317a3c36597f6eb958143d9f64f7c6410d668a8
silveryfu <silveryfu@gmail.com>
2022-02-05 14:58:47 -0800

    allow adding yaml files to driver image
    Signed-off-by: silveryfu <silveryfu@gmail.com>

--- driver/digi/state.py
+++ driver/digi/state.py
@@ -81,14 +81,10 @@ class Model():
         return digi.rc.view()
 
     def patch(self, view):
-        _ = [util.update(view, attr, None, create=False) for attr in
-             {
-                 "control.intent",
-                 "data.input",
-                 "meta",
-             }]
-        util.patch_spec(digi.g, digi.v, digi.r,
+        _, e = util.patch_spec(digi.g, digi.v, digi.r,
                         digi.n, digi.ns, view)
+        if e != None:
+            digi.logger.info(f"patch error: {e}")
 
 
 def create_model():
