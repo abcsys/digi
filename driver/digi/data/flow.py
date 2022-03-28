1c0d727ed73b5fd79a35dd9e323ca7b4ac98dc77
silveryfu <silveryfu@gmail.com>
2022-03-28 10:39:16 -0700

    make consistent the order of event_ts and ts fields
    Signed-off-by: silveryfu <silveryfu@gmail.com>

--- driver/digi/data/flow.py
+++ driver/digi/data/flow.py
@@ -3,7 +3,7 @@
 refresh_ts = """switch ( 
     case has(event_ts) => yield this | put ts := now()
     case has(ts) => put event_ts := ts | put ts := now() 
-    default => put ts := now() | put event_ts := ts
+    default => put event_ts := now() | put ts := event_ts
 )"""
 
 patch_ts = "switch ( case has(ts) => yield this " \
