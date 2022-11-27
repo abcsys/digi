c93986b50ab823c6add0c5b85d044a443e3b7425
Jamsheed Mistri <jmistri7@gmail.com>
2022-11-26 18:35:08 -0800

    [driver] Fix deps: python zed and durationpy (#17)

--- driver/setup.py
+++ driver/setup.py
@@ -20,8 +20,9 @@ setup(
         "python-box",
         "kubernetes",
         'kopf',
-        "zed",
+        "zed @ git+https://github.com/silveryfu/zed#subdirectory=python/zed",
         "dash",
         "pandas",
+        "durationpy",
     ],
 )
