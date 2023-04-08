97be20633bd88e52439845efeb3a4c674dbce02a
silveryfu <silveryfu@gmail.com>
2023-04-07 21:15:03 -0700

    Improve driver build speed with more cache reuse
    Signed-off-by: silveryfu <silveryfu@gmail.com>

--- driver/setup.py
+++ driver/setup.py
@@ -1,28 +1,14 @@
 from setuptools import setup, find_packages
 
-URLS = {
-}
-
 setup(
     name="digi",
     version="0.2.8",
     description="Digi driver library",
-    url=URLS,
     author="Team Digi",
     author_email="silveryfu@gmail.com",
     license="BSD-3-Clause License",
     packages=find_packages(exclude=("tests",)),
     python_requires='>=3.7',
     include_package_data=True,
-    install_requires=[
-        "pyyaml",
-        "inflection",
-        "python-box",
-        "kubernetes",
-        'kopf',
-        "dash",
-        "pandas",
-        "pyzed",
-        "paho-mqtt",
-    ],
+    install_requires = open('requirements.txt').readlines(),
 )
